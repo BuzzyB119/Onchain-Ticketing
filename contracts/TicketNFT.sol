dbc1af972e2df37ca4c18a479aceafdcb79a7fa3
BuzzyB119 <104602978+BuzzyB119@users.noreply.github.com>
2025-04-16 21:38:15 +0000

    Modified Ticketing Smart Contracts

--- contracts/TicketNFT.sol
+++ contracts/TicketNFT.sol
@@ -1,20 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract TicketNFT is ERC721, Ownable {
+contract TicketNFT is ERC721, Ownable2Step {
     uint256 private _tokenIdCounter;
 
-    constructor() ERC721("EventTicket", "TKT") Ownable(msg.sender) {}
+    event TicketMinted(address indexed to, uint256 indexed tokenId);
+    event ContractDeployed(address indexed owner);
+
+    constructor() ERC721("EventTicket", "TKT") Ownable2Step() payable {
+        _transferOwnership(msg.sender);
+        emit ContractDeployed(msg.sender);
+    }
 
     function mint(address to) public onlyOwner {
-        _tokenIdCounter += 1;
-        _safeMint(to, _tokenIdCounter);
+        require(to != address(0), "Cannot mint to zero address");
+        uint256 tokenId = _tokenIdCounter + 1; // Cache and use +
+        _tokenIdCounter = tokenId;
+        _mint(to, tokenId); // Use _mint instead of _safeMint
+        emit TicketMinted(to, tokenId);
     }
 
     function verifyOwnership(address owner, uint256 tokenId) public view returns (bool) {
+        require(owner != address(0), "Invalid owner address");
         return ownerOf(tokenId) == owner;
     }
 }
