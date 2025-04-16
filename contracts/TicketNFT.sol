7adfbdc67ebd7ee28caf6b88704fbb50fda02239
BuzzyB119 <104602978+BuzzyB119@users.noreply.github.com>
2025-04-16 23:26:17 +0000

    Smart Contract Update

--- contracts/TicketNFT.sol
+++ contracts/TicketNFT.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity 0.8.26;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TicketNFT is ERC721, Ownable2Step {
+contract TicketNFT is ERC721, Ownable {
     uint256 private _tokenIdCounter;
 
     event TicketMinted(address indexed to, uint256 indexed tokenId);
     event ContractDeployed(address indexed owner);
 
-    constructor() ERC721("EventTicket", "TKT") Ownable2Step() payable {
-        _transferOwnership(msg.sender);
+    constructor() ERC721("EventTicket", "TKT") Ownable(msg.sender) payable {
         emit ContractDeployed(msg.sender);
     }
 
     function mint(address to) public onlyOwner {
         require(to != address(0), "Cannot mint to zero address");
-        uint256 tokenId = _tokenIdCounter + 1; // Cache and use +
+        uint256 tokenId = _tokenIdCounter + 1; // Cache for gas savings
         _tokenIdCounter = tokenId;
-        _mint(to, tokenId); // Use _mint instead of _safeMint
+        _mint(to, tokenId); // Avoids reentrancy
         emit TicketMinted(to, tokenId);
     }
 
