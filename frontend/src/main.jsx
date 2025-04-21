5d6b554b95e949957d7104751cd73c34bba7c016
BuzzyB119 <104602978+BuzzyB119@users.noreply.github.com>
2025-04-21 02:51:25 +0000

    Updated main.jsx for wallet compatibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { WagmiProvider, createConfig } from 'wagmi';
+import { WagmiProvider, createConfig, http } from 'wagmi';
 import { baseSepolia } from 'wagmi/chains';
-import { http } from 'viem';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { OnchainKitProvider } from '@coinbase/onchainkit';
 import App from './App.jsx';
 import './index.css';
 
@@ -22,12 +20,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider apiKey="" chain={baseSepolia}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </OnchainKitProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </QueryClientProvider>
     </WagmiProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
